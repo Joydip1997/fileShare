a55996714ab996d2859408bb61eecdff6a878420
Joydip1997 <51002995+Joydip1997@users.noreply.github.com>
2020-09-04 12:17:59 +0530

    initial

--- Routes/downloadfile.js
+++ Routes/downloadfile.js
@@ -2,30 +2,22 @@ const Router = require("express").Router();
 const File = require('../Models/file');
 
 
-Router.get('/:uuid', async(req, res) => {
-    console.log(req.params)
+Router.get("/:uuid", async(req, res) => {
+
 
     try {
         const file = await File.findOne({ uuid: req.params.uuid })
+
         if (!file) {
-            return res.json({ "error": "error" })
+            return res.status(500)
         }
 
-        return res.status(200).json({
-            download: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
-        });
-
-
-
-
-
+        return res.render('download.ejs', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}` });
 
     } catch (error) {
-        res.json({ "error": "inner error" })
+
     }
-
-
-});
+})
 
 
 
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = 4000
 
 
 
 const db = require('./config/db');
 db();
 
-app.use('/api/files', require("./Routes/getfile"));
+
+app.use("/api/files", require("./Routes/getfile"));
 app.use("/files", require("./Routes/downloadfile"));
 app.use("/files/download", require("./Routes/download"))
 
+app.get("/", (req, res) => {
+    return res.render('download.ejs', { uuid: "sasa", fileName: "sasa", fileSize: "sasa", downloadLink: "sasa" });
+})
 
 
 
