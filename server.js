7b5cfcfd8189edec121f898dba3987294c9342d8
Joydip1997 <51002995+Joydip1997@users.noreply.github.com>
2020-09-04 13:03:57 +0530

    initial

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const test = require("./Models/test")
 
 const PORT = process.env.PORT || 3000
 
@@ -15,6 +16,20 @@ app.use("/files/download", require("./Routes/download"))
 
 app.get("/", (req, res) => {
     return res.render('upload.ejs');
+})
+
+app.post("/show", async(req, res) => {
+    try {
+        const file = test({
+            filename: "yes",
+        })
+        const response = await file.save()
+        return res.json({ "done": "DONE" })
+
+    } catch (error) {
+
+    }
+
 })
 
 
